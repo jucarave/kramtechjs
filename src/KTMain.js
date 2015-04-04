2960ce292fea7b00f94e65d446cf02cf3416a8d0
Camilo Ramírez <jucarave92@hotmail.com>
2015-04-04 01:56:34 -0500

    * Main class

---
+++ src/KTMain.js
@@ -0,0 +1,39 @@
+module.exports = {
+	init: function(canvas){
+		this.canvas = canvas;
+		this.gl = null;
+		this.aspectRatio = 0;
+		
+		this.__initContext(canvas);
+		this.__initProperties();
+	},
+	
+	__initContext: function(canvas){
+		this.gl = canvas.getContext('experimental-webgl');
+		
+		if (!this.gl){
+			alert("Your browser doesn't support WebGL");
+			return false;
+		}
+		
+		this.gl.width = canvas.width;
+		this.gl.height = canvas.height;
+	},
+	
+	__initProperties: function(){
+		var gl = this.gl;
+		
+		gl.enable(gl.DEPTH_TEST);
+		gl.depthFunc(gl.LEQUAL);
+		
+		gl.enable(gl.CULL_FACE);
+		
+		gl.enable( gl.BLEND );
+		gl.blendEquation( gl.FUNC_ADD );
+		gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
+		
+		this.aspectRatio = this.canvas.width / this.canvas.height;
+	}
+};
+
+
--- src/KramTech.js
+++ src/KramTech.js
@@ -1,5 +1,4 @@
-var KT = {};
-
+var KT = require('./KTMain');
 KT.Math = require('./KTMath');
 KT.Vector3 = require('./KTVector3');
 KT.Matrix4 = require('./KTMatrix4');

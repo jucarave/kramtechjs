5e01fbfd52febe6f3b8c1f4b431b89ffbab1efb4
Camilo Ramírez <jucarave92@hotmail.com>
2015-04-03 22:34:42 -0500

    Math Class

---
+++ src/KTMath.js
@@ -0,0 +1,16 @@
+module.exports = {
+	radDegRel: Math.PI / 180,
+	
+	PI_2: Math.PI / 2,
+	PI: Math.PI,
+	PI3_2: Math.PI * 3 / 2,
+	PI2: Math.PI * 2,
+	
+	degToRad: function(degrees){
+		return degrees * this.radDegRel;
+	},
+	
+	radToDeg: function(radians){
+		return radians / this.radDegRel;
+	}
+};
--- src/KramTech.js
+++ src/KramTech.js
@@ -1,5 +1,6 @@
 var KT = {};
 
 KT.Vector3 = require('./KTVector3');
+KT.Math = require('./KTMath');
 
 module.exports = KT;

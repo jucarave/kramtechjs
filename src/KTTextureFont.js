2f326c856407033b55ecfedd04d10d5faa3c062e
Camilo Ramírez <jucarave92@hotmail.com>
2015-05-28 07:00:54 -0500

    Convert font image to imageData

--- src/KTTextureFont.js
+++ src/KTTextureFont.js
@@ -18,11 +18,37 @@ function TextureFont(imageSrc, charWidth, charHeight, charList){
 	var T = this;
 	this.image = KT.loadImage(imageSrc, function(sprite){
 		T.hCharNum = sprite.width / T.charWidth;
+		T.parseImageData();
 		T.parseTexture();
 	});
 }
 
 module.exports = TextureFont;
+
+TextureFont.prototype.parseImageData = function(){
+	var cnv = document.createElement("canvas");
+	cnv.width = this.image.width;
+	cnv.height = this.image.height;
+	
+	var ctx = cnv.getContext("2d");
+	ctx.drawImage(this.image, 0, 0);
+	
+	var imageData = ctx.getImageData(0,0,this.image.width,this.image.height);
+	var dataArr = [];
+	for (var i=0,len=imageData.data.length;i<len;i+=4){
+		var r = imageData.data[i];
+		var g = imageData.data[i+1];
+		var b = imageData.data[i+2];
+		var a = imageData.data[i+3];
+		
+		if (r == 255 && g == 0 && b == 255)
+			a = 0;
+		
+		dataArr.push(r, g, b, a);
+	}
+	
+	this.imageData = new Uint8Array(dataArr);
+};
 
 TextureFont.prototype.parseTexture = function(){
 	var gl = KT.gl;
@@ -30,9 +56,8 @@ TextureFont.prototype.parseTexture = function(){
 	this.texture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, this.texture);
 	
-	//gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-	
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+	//gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.image.width, this.image.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.imageData);
 	
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
